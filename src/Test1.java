45a1fedaedd8c569358b71f636df6f58d8e268ae
Talha Ozleblebici <talha43gs@gmail.com>
2023-09-18 21:02:01 +0100

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
         System.out.println("ADDED LOCALLY, pushed with GIT MENU!!!");
         // BURASI REMOTE REPODA GITHUBDA EKLENDI!!!!
         // YENI BRANCH DA BU SATIR EKLENDI (GS-1905_Login_Test)
+        // remote da eklendi !!!!
     }
 }
